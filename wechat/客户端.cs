74f9017562ea626288c6d3f4912dce25fcc1bc29
Liukaiyan <943222628@qq.com>
2017-03-07 16:01:41 +0800

    客户端和服务端代码。服务端不稳定。但是能够勉强实现群聊功能。暂时有两个bug。一个是一方发送消息之后消息会循环出现，从界面上来看收到了无数条一样的消息。另一个bug是启动服务端的电脑会产生错误 弹出 变量 hostname 不能为null

---
+++ wechat/客户端.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private const int Port = 888;                       //指定端口号
+        private const int serPort = 999;
+        private Thread td;                                  //声明监听线程
+        private TcpListener tcpListener;                    //声明监听对象
+        private static string message = "";                 //定义发送和接收的消息
+        IPAddress[] ips = Dns.GetHostAddresses(Dns.GetHostName());      //获取本地主机ip
+
+        private void Form1_Load(object sender,EventArgs e)
+        {
+            td = new Thread(new ThreadStart(this.StartListen));
+            td.Start();
+            timer1.Start();
+        }
+        private void StartListen()
+        {
+            message = "";
+            tcpListener = new TcpListener(Port);
+            tcpListener.Start();
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = tcpListener.AcceptTcpClient();
+                    byte[] mbyte = new byte[1024];
+                    NetworkStream ntwstream = client.GetStream();
+                    int i = ntwstream.Read(mbyte, 0, mbyte.Length);
+                    message = Encoding.Default.GetString(mbyte, 0, i);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        /*发送信息按钮*/
+        private void button1_Click(object sender,EventArgs e)
+        {
+            try
+            {                
+                string strmsg = "" + "(" + ips[1].ToString() + ")" + DateTime.Now.ToLongTimeString() + "\n" + " " + this.rtbSend.Text + "\n";
+
+                TcpClient client = new TcpClient(txtIP.Text, Port);      //创建tcpclient对象
+                NetworkStream ntwstream = client.GetStream();                   //创建网络流
+                StreamWriter wstream = new StreamWriter(ntwstream, Encoding.Default);
+                wstream.Write(strmsg);                                          //将消息写入网络流
+
+                wstream.Flush();
+                wstream.Close();
+                client.Close();                                                 //关闭服务，释放对象
+
+                rtbContent.AppendText(strmsg);
+                rtbContent.ScrollToCaret();
+                rtbSend.Clear();                                                //用户界面显示处理
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /*连接服务器按钮,发送自己的ip*/
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TcpClient client = new TcpClient(txtIP.Text, serPort);
+                if (client != null)
+                {
+                    NetworkStream ntwstream = client.GetStream();                   //创建网络流
+                    StreamWriter wstream = new StreamWriter(ntwstream, Encoding.Default);
+
+
+                    //string hostIP = ips[5].ToString();
+                    string hostIP;
+                    foreach(IPAddress hostIPAddress in ips)
+                    {
+                        hostIP = hostIPAddress.ToString();
+                        if (Regex.IsMatch(hostIP, @"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"))
+                        {
+                            if (Regex.IsMatch(hostIP, "192")) {  }
+
+                            else { wstream.Write(hostIP); }          //将消息写入网络流
+                        }
+                    }
+                    wstream.Flush();
+                    wstream.Close();
+
+                    MessageBox.Show("连接成功");
+                    client.Close();
+                }
+            }
+            catch
+            { 
+                MessageBox.Show("连接失败请尝试在其他时间连接");
+            }
+
+        }
+
+        //计时器tick事件
+        private void timer1_Tick(object sender,EventArgs e)
+        {
+            if (message != "")
+            {
+                rtbContent.AppendText(message);
+                rtbContent.ScrollToCaret();
+                message = "";
+            }
+        }
+
+        private void Form1_FormClosed(object sender,FormClosedEventArgs e)
+        {
+            if (this.tcpListener != null) { tcpListener.Stop(); }
+            if (td != null)
+            {
+                if (td.ThreadState == ThreadState.Running) { td.Abort(); }
+            }
+        }
+
+    }
+}
---
+++ wechat/服务端.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private const int MAXCONNECT = 50;
+        private const int Port = 888;                       //指定端口号
+        private const int ipPort = 999;                     //指定监听 ip 地址的端口号
+        private Thread td;                                  //声明监听线程
+        private Thread connect;                             //声明连接线程
+        private TcpListener tcpListener;                    //声明监听对象
+        private TcpListener tcpListenerIp;                  //声明监听 ip 对象
+        private static string message = "";                 //定义发送和接收的消息
+        string[] ips = new string[MAXCONNECT];              //定义存储各个成员ip地址的数组
+        private static string eachIP = "";                  //定义接收每个成员的ip
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)  //窗口加载
+        {
+            td = new Thread(new ThreadStart(this.StartListen)); //创建监听线程指定方法
+            td.Start();                                         //线程启动
+            connect = new Thread(new ThreadStart(this.getIP)); //创建连接线程指定方法
+            connect.Start();                                         //线程启动
+            timer1.Start();                                     //计时器启动
+        }
+        private void StartListen()                              //线程监听方法
+        {
+            message = "";
+            tcpListener = new TcpListener(Port);
+            tcpListener.Start();
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = tcpListener.AcceptTcpClient();      //接受连接请求
+                    byte[] mbyte = new byte[1024];                          //建立缓存
+                    NetworkStream ntwstream = client.GetStream();          //从连接获取数据流
+                    int i = ntwstream.Read(mbyte, 0, mbyte.Length);         //将数据流写入缓存
+                    message = Encoding.Default.GetString(mbyte, 0, i);      //记录收到的消息
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                 }
+            }
+        }
+        /*定义连接时获取所有客户端的ip*/
+        private void getIP()
+        {
+            eachIP = "";
+            tcpListenerIp = new TcpListener(ipPort);
+            tcpListenerIp.Start();
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = tcpListenerIp.AcceptTcpClient();
+                    byte[] mbyte = new byte[1024];
+                    NetworkStream ntwstream = client.GetStream();
+                    int i = ntwstream.Read(mbyte, 0, mbyte.Length);
+                    eachIP = Encoding.Default.GetString(mbyte, 0, i);
+                    ntwstream.Flush();
+                    ntwstream.Close();
+                    client.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+
+
+        }
+        /*启动计时器，判断当前是否有消息传输*/
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(eachIP != "")
+            {
+                for (int j = 0; j <= MAXCONNECT; j++)
+                {
+                    if (ips[j] == null)
+                    {
+                        ips[j] = eachIP;
+                        break;
+                    }
+                }
+                connectIP.AppendText(eachIP);
+                eachIP = "";
+            }
+            if (message != "")
+            {
+                /*发送消息给每一个客户端*/
+                try
+                {
+                    foreach (string ip in ips)
+                    {
+                        TcpClient client = new TcpClient(ip, Port);      //创建tcpclient对象
+                        NetworkStream ntwstream = client.GetStream();                   //创建网络流
+                        StreamWriter wstream = new StreamWriter(ntwstream, Encoding.Default);
+                        wstream.Write(message);                                          //将消息写入网络流
+
+                        wstream.Flush();
+                        wstream.Close();
+                        client.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                chatContent.AppendText(message);
+                chatContent.ScrollToCaret();
+                message = "";
+            }
+        }
+
+        
+        
+        /*关闭窗体，停止所有服务*/
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (this.tcpListener != null) { tcpListener.Stop(); }
+            if (this.tcpListenerIp != null) { tcpListenerIp.Stop(); }
+            if (td != null)
+            {
+                if (td.ThreadState == ThreadState.Running) { td.Abort(); }
+            }
+            if (connect != null)
+            {
+                if (connect.ThreadState == ThreadState.Running) { connect.Abort(); }
+            }
+        }
+    }
+}
