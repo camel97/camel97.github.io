fa119cd04106105059b23fe8dbf63df7331eae48
Liukaiyan <943222628@qq.com>
2017-03-09 21:51:05 +0800

    这应该是最终的代码了。暂时能够实现双方聊天。显示各个客户端的 ip ，保存聊天记录。在用户断网的情况下可以将用户没有接收到的信息（聊天记录）保存在本地文件中。

--- wechat/服务端.cs
+++ wechat/服务端.cs
@@ -40,14 +40,14 @@ namespace WindowsFormsApplication2
             td = new Thread(new ThreadStart(this.StartListen)); //创建监听线程指定方法
             td.Start();                                         //线程启动
             connect = new Thread(new ThreadStart(this.getIP)); //创建连接线程指定方法
-            connect.Start();                                         //线程启动
+            connect.Start();                                     //线程启动
             timer1.Start();                                     //计时器启动
             timer2.Start();
         }
         private void StartListen()                              //线程监听方法
         {
             message = "";
-            tcpListener = new TcpListener(hostname[5],Port);
+            tcpListener = new TcpListener(Port);
             tcpListener.Start();
             while (true)
             {
@@ -69,17 +69,17 @@ namespace WindowsFormsApplication2
         private void getIP()
         {
             eachIP = "";
-            tcpListenerIp = new TcpListener(hostname[5],ipPort);
+            tcpListenerIp = new TcpListener(ipPort);
             tcpListenerIp.Start();
             while (true)
             {
                 try
                 {
-                    TcpClient client = tcpListenerIp.AcceptTcpClient();
-                    byte[] mbyte = new byte[1024];
-                    NetworkStream ntwstream = client.GetStream();
+                    TcpClient client = tcpListenerIp.AcceptTcpClient();             //建立连接请求
+                    byte[] mbyte = new byte[1024];                                  //建立缓存
+                    NetworkStream ntwstream = client.GetStream();                   //写入缓存
                     int i = ntwstream.Read(mbyte, 0, mbyte.Length);
-                    eachIP = Encoding.Default.GetString(mbyte, 0, i);
+                    eachIP = Encoding.Default.GetString(mbyte, 0, i);               //得到每个客户端的 ip
                     ntwstream.Flush();
                     ntwstream.Close();
                     client.Close();
@@ -96,8 +96,9 @@ namespace WindowsFormsApplication2
         /*启动计时器，判断当前是否有消息传输*/
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if(eachIP != "")
+            if(eachIP != "")                                            //将客户端的 ip 添加到 ips 组里
             {
+                if (!File.Exists(eachIP+".txt")) { using (File.Create(eachIP + ".txt")) ;  }
                 for (int j = 0; j <= MAXCONNECT; j++)
                 {
                     if (ips[j] == null)
@@ -106,16 +107,18 @@ namespace WindowsFormsApplication2
                         break;
                     }
                 }
-                connectIP.AppendText(eachIP+"\n");
-                connectIP.AppendText("\n");
+                
+
+                connectIP.AppendText(eachIP+"\n");              //右边方框显示当前连接的所有客户端的  ip
                 eachIP = "";
             }
             if (message != "")
             {
                 /*发送消息给每一个客户端*/
-                try
+                
+                foreach (string ip in ips)              //从 ips 组中取到所有的客户端 ip
                 {
-                    foreach (string ip in ips)
+                    try
                     {
                         TcpClient client = new TcpClient(ip, Port);      //创建tcpclient对象
                         NetworkStream ntwstream = client.GetStream();                   //创建网络流
@@ -126,29 +129,39 @@ namespace WindowsFormsApplication2
                         wstream.Close();
                         client.Close();
                     }
-                }
-                catch (Exception ex)
-                {
-                    
-                }
+                    catch (Exception)                                       //在这里做了异常处理，在截取异常之后，会出现服务端暂时卡死，同时目录下新建了一个
+                                                                            //  .txt 文件自动重复多次写入聊天记录。弄不懂。
+                    {                        
+                        try
+                        {
+                            using (StreamWriter swriter = new StreamWriter(ip+".txt", true)) { swriter.WriteLine(message); }//断线时将消息保存在文件中
+                        }
+                        catch(Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }                    
+                    }
+                }                               
                 chatContent.AppendText(message);
                 chatContent.ScrollToCaret();
                 message = "";
             }
         }
+       
+
+
         /*计时器用来保存聊天记录*/
-        private void timer2_Tick(object sender,EventArgs e)
+        private void timer2_Tick(object sender,EventArgs e)    //每一分钟将聊天框的内容保存在文本中同时清屏
         {
             //string chatRecords = chatContent.Text;
             try
                 {
-                    if (!File.Exists("Log.txt"))
-                    {
-                        //MessageBox.Show("create");
-                        File.Create("Log.txt");
+                    if (!File.Exists("Log.txt"))                //创建聊天记录的保存文本
+                    {                   
+                    using (File.Create("Log.txt")) ;
                     }
-                    using (StreamWriter swriter = new StreamWriter("Log.txt", true)) { swriter.WriteLine(chatContent.Text); }
-                    chatContent.Clear();
+                    using (StreamWriter swriter = new StreamWriter("Log.txt", true)) { swriter.WriteLine(chatContent.Text); }//写入聊天记录
+                    chatContent.Clear();                        //清屏
 
                 }
                 catch (Exception ex)
@@ -159,20 +172,19 @@ namespace WindowsFormsApplication2
         }
 
 
-
         /*关闭窗体，停止所有服务*/
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
             if (this.tcpListener != null) { tcpListener.Stop(); }
-            if (this.tcpListenerIp != null) { tcpListenerIp.Stop(); }
-            if (td != null)
+            if (this.tcpListenerIp != null) { tcpListenerIp.Stop(); }       //停止侦听
+            if (td != null)                                             
             {
                 if (td.ThreadState == ThreadState.Running) { td.Abort(); }
             }
             if (connect != null)
             {
                 if (connect.ThreadState == ThreadState.Running) { connect.Abort(); }
-            }
+            }                                                                           //关闭线程
         }
     }
 }
